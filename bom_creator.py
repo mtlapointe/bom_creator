c50fe6a8458f03ea3743a618d23de08c9620ffe8
MattLaPointe <matt.lapointe@dss-space.com>
2019-01-20 22:09:43 -0800

    Added drawing BOM

--- bom_creator.py
+++ bom_creator.py
@@ -15,19 +15,33 @@ if '.csv' in file_name:
 
 bom = bomloader.BOM().load_csv(csv_file_path)
 
-cols = ['Level', 'Depth', 'Type', 'Part Number', 'Description', 'QTY', 'Total QTY', 'Used On', 'Cage Code',
+export = dfexporter.DFExport(f'{bom.get_date_from_file()} {bom.get_assy_from_file()}.xlsx')
+
+full_bom_cols = ['Level', 'Depth', 'Type', 'Part Number', 'Description', 'QTY', 'Total QTY', 'Used On', 'Cage Code',
         'Revision', 'Drawing', 'Drawing Number', 'Duplicate', 'Material', 'Finish 1', 'Finish 2', 'Finish 3',
         'Weight', 'State', 'Latest Version']
 
-export = dfexporter.DFExport(f'{bom.get_date_from_file()} {bom.get_assy_from_file()}.xlsx')
 export.add_sheet(bom.df,
-                 sheet_name='Highlight No Index',
+                 sheet_name='Full BOM',
                  freeze_col=10, freeze_row=1,
-                 cols_to_print=cols,
-                 depth_col_name='Depth', group_rows=True,
-                 highlight_rows=True, highlight_col_limit=0,
+                 cols_to_print=full_bom_cols,
+                 depth_col_name='Depth',
+                 group_rows=True,
+                 highlight_depth=True,
+                 highlight_col_limit=0,
                  cols_to_indent=['Part Number'],
                  print_index=True)
+
+drw_df = bom.df.drop_duplicates('Drawing Number').sort_values('Drawing Number')
+
+drw_bom_cols = ['Drawing Number', 'Description', 'State', 'Revision',  'Latest Version']
+
+export.add_sheet(drw_df,
+                 sheet_name='Drawings',
+                 cols_to_print=drw_bom_cols,
+                 print_index=False)
+
+export.add_raw_sheet(drw_df, 'Debug')
 
 export.write_book()
 
